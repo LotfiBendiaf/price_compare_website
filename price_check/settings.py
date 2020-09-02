dc80a127572ca523ba4d91aadabe5b64a4dd54b0
Lotfi <lotfi.m.bendiaf@gmail.com>
2020-09-02 07:19:08 +0100

    'Price_compare_final'

--- price_check/settings.py
+++ price_check/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
