b0e29849e02b94a8848c1d9fd1304318ff78fdf3
Lotfi <lotfi.m.bendiaf@gmail.com>
2020-09-03 17:54:11 +0100

    'Responsive-app'

--- price_check_app/admin.py
+++ price_check_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Search
+
+
+admin.site.site_header = "My Price Check"
+admin.site.site_title = "My Price Check"
+admin.site.index_title = "My Price Check Admin"
+
 # Register your models here.
+admin.site.register(Search)
